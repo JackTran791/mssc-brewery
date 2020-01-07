39a8ac5ef19b8c66aa41fe9a050784cc5c2e73eb
JackTran791 <congsonit@gmail.com>
2020-01-07 15:41:39 -0600

    - Add @RequestBody in POST and PUT methods.

--- src/main/java/com/luv2code/msscbrewwery/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/luv2code/msscbrewwery/msscbrewery/web/controller/BeerController.java
@@ -28,12 +28,18 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity handlePost(BeerDto beerDto) {
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto) {
         BeerDto savedDto = beerService.savedNewBeer(beerDto);
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
 
         return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        beerService.updateBeer(beerId, beerDto);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
