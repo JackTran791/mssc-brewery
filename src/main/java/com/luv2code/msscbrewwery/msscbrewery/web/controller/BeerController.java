d24a46e9b5d3bd91f0bb6caea1ad5bce602201aa
JackTran791 <congsonit@gmail.com>
2020-01-07 12:33:49 -0600

    - Add Data Model - Add MVC Controller

---
+++ src/main/java/com/luv2code/msscbrewwery/msscbrewery/web/controller/BeerController.java
@@ -0,0 +1,24 @@
+package com.luv2code.msscbrewwery.msscbrewery.web.controller;
+
+import com.luv2code.msscbrewwery.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author Jack Tran
+ */
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping({"/{beerId}"})
+    public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId")UUID beerId) {
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+}
